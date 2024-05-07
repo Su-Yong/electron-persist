00055e75f1e393942054987f75a539204e161cc1
Su-Yong <simssy2205@gmail.com>
2024-05-07 13:14:05 +0900

    feat: Setup workspace

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es', 'umd'],
+      name: 'electronPersistCore',
+      fileName: 'index',
+    },
+    outDir: 'dist',
+  },
+  plugins: [
+    dts({
+      rollupTypes: true,
+    }),
+  ],
+});
