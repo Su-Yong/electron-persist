f99d9c97d2a0dd69266dfc12a2c5f84c2955cfe1
Su-Yong <simssy2205@gmail.com>
2024-06-09 18:42:25 +0900

    feat(migrator): design migrator interface & add `versionField` in FilePersister

--- packages/core/src/persister/FilePersister.ts
+++ packages/core/src/persister/FilePersister.ts
@@ -1,9 +1,11 @@
 import fs from 'node:fs/promises';
 
 import { Persister, PersisterOptions } from './Persister';
+import { isDev } from '../util';
 
 export interface FilePersisterOptions<T> extends PersisterOptions<T> {
   path: string;
+  versionField?: string;
   serializer?: typeof JSON.stringify,
   deserializer?: typeof JSON.parse,
 }
@@ -12,6 +14,7 @@ export class FilePersister<T> extends Persister<T> {
   private path: string;
   private serializer: typeof JSON.stringify = JSON.stringify;
   private deserializer: typeof JSON.parse = JSON.parse;
+  private versionField = '__version__';
 
   constructor(path: string);
   constructor(options: FilePersisterOptions<T>);
@@ -24,6 +27,7 @@ export class FilePersister<T> extends Persister<T> {
       this.path = options;
     } else {
       this.path = options.path;
+      this.versionField = options.versionField ?? '__version__';
       this.serializer = options.serializer ?? JSON.stringify;
       this.deserializer = options.deserializer ?? JSON.parse;
     }
@@ -31,10 +35,28 @@ export class FilePersister<T> extends Persister<T> {
 
   async read() {
     const str = await fs.readFile(this.path, 'utf-8');
-    return this.deserializer(str);
+    const result = this.deserializer(str);
+
+    if (typeof result === 'object') delete result[this.versionField];
+    else if (isDev) console.warn('The config file is not an object. Please make sure it is a valid JSON object.');
+
+    return result;
   }
 
   async write(data: T) {
-    await fs.writeFile(this.path, this.serializer(data), 'utf-8');
+    if (isDev) {
+      if (data && typeof data === 'object' && this.versionField in data) {
+        console.warn(`The field "${this.versionField}" is reserved for the version of the config. Please remove it from your config.`);
+      }
+      if (!this.version) {
+        console.warn('No version detected. Please set a version for the Application.');
+      }
+    }
+
+    const str = this.serializer({
+      ...data,
+      [this.versionField]: this.version,
+    });
+    await fs.writeFile(this.path, str, 'utf-8');
   }
 }
