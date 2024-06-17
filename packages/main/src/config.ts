5aac42a7740ffa0d48abb04835f8f77723012bdc
Su-Yong <simssy2205@gmail.com>
2024-06-17 12:00:31 +0900

    feat(renderer): add `IpcPersister`

--- packages/main/src/config.ts
+++ packages/main/src/config.ts
@@ -1,9 +1,27 @@
 import { Config } from '@electron-persist/core';
 import { Channel } from '@electron-persist/shared';
 
-import { ipcMain } from 'electron';
+import { ipcMain, BrowserWindow } from 'electron';
 
-export const registerConfig = <K extends string, T>(name: K, config: Config<T>) => {
+export const registerConfig = <K extends string, T>(win: BrowserWindow, name: K, config: Config<T>) => {
+  const windows: BrowserWindow[] = [];
+
   ipcMain.handle(Channel.GET(name), async (_, key) => config.get(key));
   ipcMain.handle(Channel.SET(name), async (_, key, value) => config.set(key, value));
+
+  config.watchAll((value) => {
+    windows.forEach((win) => win.webContents.send(Channel.SET(name), value));
+  });
+
+  return {
+    addWindow: (win: BrowserWindow) => {
+      windows.push(win);
+    },
+    removeWindow: (win: BrowserWindow) => {
+      const index = windows.indexOf(win);
+      if (index !== -1) {
+        windows.splice(index, 1);
+      }
+    },
+  };
 };
