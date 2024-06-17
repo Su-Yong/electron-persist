9f362679191af4dcd42620336e3c8280fcefc246
Su-Yong <simssy2205@gmail.com>
2024-06-17 10:38:22 +0900

    feat(renderer): split packages as `shared`, `renderer`

--- packages/main/src/config.ts
+++ packages/main/src/config.ts
@@ -1,8 +1,7 @@
 import { Config } from '@electron-persist/core';
+import { Channel } from '@electron-persist/shared';
 
 import { ipcMain } from 'electron';
-
-import { Channel } from './key';
 
 export const registerConfig = <K extends string, T>(name: K, config: Config<T>) => {
   ipcMain.handle(Channel.GET(name), async (_, key) => config.get(key));
